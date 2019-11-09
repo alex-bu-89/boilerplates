fe22c5570a70b3a492a853e29cb4a44244cd5dda
Alex Buyanov <alex.bu.89@gmail.com>
2019-11-09 16:30:15 +0100

    [express-typescript-api] small config changes

--- express-typescript-api/config/default.js
+++ express-typescript-api/config/default.js
@@ -3,10 +3,10 @@ module.exports = {
     port: process.env.PORT || 3000,
     serviceName: 'express-typescript-boilerplate',
     logger: {
-        level: process.env.LOG_LEVEL || 'trace',
+        level: 'trace',
     },
     db: {
-        host: process.env.MONGODB_HOST || 'mongodb://localhost:27017',
-        name: process.env.MONGODB_DBNAME || 'mydb',
+        host: 'mongodb://localhost:27017',
+        name: 'mydb',
     }
 }
